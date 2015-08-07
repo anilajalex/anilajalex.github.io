f1943c3f6634f5688dda8bb7c1561f87b4a5d581
Anila Alexander <anila.j.alexander@gmail.com>
2015-08-07 01:07:47 -0400

    added clips page

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ $( document ).ready(function() {
     // SCROLL TO TOP
    $("#top").click(function(){
       $('html, body').animate({ scrollTop: 0 }, 'slow');
-		})
+		});
+   $(".hi").ready(function(){
+   	console.log("loading"); 
+   }); 
 });
