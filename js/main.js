33bb48ea2f529dc26f5c8eeb3d7fb45dc921e64a
Anila Alexander <anila.j.alexander@gmail.com>
2016-01-10 20:06:21 -0500

    beginnings of redesign

--- js/main.js
+++ js/main.js
@@ -1,9 +1,139 @@
 $( document ).ready(function() {
-    // SCROLL TO TOP
-   $("#top").click(function(){
-      $('html, body').animate({ scrollTop: 0 }, 'slow');
-		});
-   $(".hi").ready(function(){
-   	console.log("loading"); 
-   }); 
+
+
+//Particles
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#0a0102"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#0a0102"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#0a0102",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
 });
+
+
+	//Changing Text
+
+	var text = ["Full-Stack Web Developer", "Interactive Storyteller", "Journalist", "Cheesecake Enthusiast", "Traveler", "Designer"];
+    var counter = 0;
+    var elem = document.getElementById("changeText");
+    setInterval(change, 1000);
+
+    function change() {
+     elem.innerHTML = text[counter];
+        counter++;
+        if(counter >= text.length) { counter = 0; }
+    }
+
+
+ //    // SCROLL TO TOP
+ //   $("#top").click(function(){
+ //      $('html, body').animate({ scrollTop: 0 }, 'slow');
+	// 	});
+ //   $(".hi").ready(function(){
+ //   	console.log("loading"); 
+ //   }); 
+
+
+   
+
+
+}); // ends doc.ready
